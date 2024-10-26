86c124405a7222c3077c6c0237845c1b1ef790b5
Muhammad-Shah-zaib <MuhammdShahzaib@outlook.com>
2024-10-26 21:43:40 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using expense_tracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register the DbContext with dependency injection
+builder.Services.AddDbContext<ExpenseTrackerContext>(options =>
+{
+    // connections string is configured in user-secrets, hence you don't need to hard-code it here or in appsettings.json
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
